bb9c3685d16c8631edc3178b90767d9bf8f920f2
MaxHorstmann <max.horstmann@gmail.com>
2013-11-05 23:35:22 -0500

    web server

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -1,3 +1,13 @@
+import "package:express/express.dart";
+
 void main() {
-  print("Hello, World!");
+  print("Starting up server...");
+  
+  var app = new Express()
+    ..get('/', (ctx){
+      ctx.render('index', {'title': 'Home'});
+    });
+  
+  app.listen("127.0.0.1", 8000);
+  
 }
