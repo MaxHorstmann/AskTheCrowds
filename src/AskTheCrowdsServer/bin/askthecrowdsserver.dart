1e070c17378bda63b9c63ac9030f96ea132681bf
Max Horstmann <mhorstmann@stackoverflow.com>
2013-11-07 14:02:04 -0500

    server

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 void main() {
   HttpServer.bind('127.0.0.1', 80).then((server) {
     server.listen((HttpRequest request) {
-      request.response.write('Hello, world');
+      request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
     });
   });
+  
+  print("listing on 127.0.0.1....");
+  
 }
