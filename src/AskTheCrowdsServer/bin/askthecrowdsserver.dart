760f15602fb249d3549c1af93b69f2e084ae5c39
Max Horstmann <mhorstmann@stackoverflow.com>
2013-11-07 14:39:21 -0500

    print out timestamp on commit

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -5,6 +5,7 @@ void main() {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
+      print(new DateTime.now());
     });
   });
   
