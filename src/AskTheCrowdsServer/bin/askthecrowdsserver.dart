c8d2327f7e4754c7512e83f951ab84c375f861ea
Max Horstmann <mhorstmann@stackoverflow.com>
2013-11-07 14:40:42 -0500

    use port 8977 everywhere

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  HttpServer.bind(InternetAddress.ANY_IP_V4, 80).then((server) {
+  HttpServer.bind(InternetAddress.ANY_IP_V4, 8977).then((server) {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
