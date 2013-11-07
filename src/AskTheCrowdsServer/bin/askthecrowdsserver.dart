f73020d7cb5fb2dcb11b9ee1a5fd979012151075
Max Horstmann <mhorstmann@stackoverflow.com>
2013-11-07 14:20:46 -0500

    bind to all IPs

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 void main() {
-  HttpServer.bind('127.0.0.1', 80).then((server) {
+  HttpServer.bind(InternetAddress.ANY_IP_V4, 80).then((server) {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
     });
   });
   
-  print("listing on 127.0.0.1....");
+  print("listing....");
   
 }
