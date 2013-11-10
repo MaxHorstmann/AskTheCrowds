6818612439029f4ee06c831b64df86a0de2277a7
Max Horstmann <max.horstmann@gmail.com>
2013-11-10 16:47:42 -0500

    noDb

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -4,19 +4,21 @@ import 'dart:io';
 void main() {
   var connectionStringRedis = "127.0.0.1:6397/0";
 
+  RedisClient.connect(connectionStringRedis)
+    .then((RedisClient client) {
+
+      client.set("test", "value")
+        .then((_) => client.get("test"))
+          .then((value) => print("redis success: $value"));
+    });      
+
+  
   HttpServer.bind(InternetAddress.ANY_IP_V4, 80).then((server) {
     server.listen((HttpRequest request) {
       
       request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
       print(new DateTime.now());
-      RedisClient.connect(connectionStringRedis)
-        .then((RedisClient client) {
-
-          client.set("test", "value")
-            .then((_) => client.get("test"))
-              .then((value) => print("redis success: $value"));
-        });      
       
     });
   }); 
