f4be6ed94d4b99f1410a438de1cb37dc6b6d4346
Max Horstmann <max.horstmann@gmail.com>
2013-11-10 08:03:38 -0500

    redis

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -1,18 +1,25 @@
+import "package:redis_client/redis_client.dart";
 import 'dart:io';
 
 void main() {
+  var connectionStringRedis = "127.0.0.1:6379/37";
+
   HttpServer.bind(InternetAddress.ANY_IP_V4, 8977).then((server) {
     server.listen((HttpRequest request) {
       request.response.write('Hello, world. Ask the Crowds server here.');
       request.response.close();
       print(new DateTime.now());
-      print(request.connectionInfo.remoteAddress);
-      print(request.method);
-      print(request.headers.toString());
-      print("--------------");      
+      RedisClient.connect(connectionStringRedis)
+        .then((RedisClient client) {
+
+          client.set("test", "value")
+            .then((_) => client.get("test"))
+              .then((value) => print("redis success: $value"));
+        });      
+      
     });
-  });
+  }); 
   
-  print("listing....");
+  print("listing...."); 
   
 }
