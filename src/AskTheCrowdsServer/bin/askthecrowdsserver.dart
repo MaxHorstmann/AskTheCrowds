dc9c528cc5dab5c6473788ddf3d010dc4a08cfbd
Max Horstmann <max.horstmann@gmail.com>
2013-11-10 20:50:38 -0500

    'redis

--- src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
+++ src/AskTheCrowdsServer/bin/askthecrowdsserver.dart
@@ -1,28 +1,76 @@
 import "package:redis_client/redis_client.dart";
 import 'dart:io';
+import 'dart:convert';
+
 
 void main() {
   var connectionStringRedis = "127.0.0.1:6379/0";
+  
+  RedisClient.connect(connectionStringRedis)
+          .then((RedisClient client) {
+            var categories = "{ categories : ['sports', 'travel', 'politics'] }";
+            client.set("categories", categories).then((_) => print("categories set"));
+          });
 
   HttpServer.bind(InternetAddress.ANY_IP_V4, 80).then((server) {
     server.listen((HttpRequest request) {
-      
-      request.response.write('Hello, world. Ask the Crowds server here.');
-      request.response.close();
-      var now = new DateTime.now();
-      print(now);
-      RedisClient.connect(connectionStringRedis)
-        .then((RedisClient client) {
-          client.set("last", now.toString())
-            .then((_) => client.get("last"))
-              .then((value) => print("redis success: $value"));
-        });      
 
-
+      print(request.uri.path);
       
+      if (request.uri.path == "/") {
+        request.response.statusCode = 200;
+        request.response.write("Ask the Crowds server.");
+        request.response.close();
+        return;
+      }
+      
+      if ((request.uri.path == "/register") && (request.method == "POST")) {
+        var data = request.toString();
+        Map user = JSON.decode(data);
+        var userGuid = user["userGuid"];
+        RedisClient.connect(connectionStringRedis)
+          .then((RedisClient client) {
+            client.sismember("users", userGuid).then((bool alreadyExists) {
+              if (alreadyExists)  {
+                request.response.statusCode = 400;
+                request.response.headers.contentType = ContentType.parse("text/json"); 
+                request.response.write("{ result = 'already exists' }");
+                request.response.close();
+              }
+              else {
+                client.sadd("users", userGuid).then((_) {
+                request.response.statusCode = 201;
+                request.response.headers.contentType = ContentType.parse("text/json"); 
+                request.response.write("{ result = 'success' }");
+                request.response.close();
+                });            
+              }
+            });
+          });        
+        return;
+      }
+      
+      if (request.uri.path == "/categories") {
+        RedisClient.connect(connectionStringRedis)
+          .then((RedisClient client) {
+            client.get("categories").then((categories) {
+              request.response.headers.contentType = ContentType.parse("text/json"); 
+              request.response.write(categories);
+              request.response.close();
+            });
+          });        
+        return;
+      }
+      
+      request.response.statusCode = 404;
+      request.response.write("404 not found");
+      request.response.close();      
+      
+
     });
   }); 
   
   print("listing...."); 
   
 }
+
