6f602cdb31e6b0d5994360b4fda94e2eb6d67dfc
Max Horstmann <max.horstmann@gmail.com>
2013-12-27 07:25:09 -0500

    fragments

--- src/AndroidClient/src/net/maxhorstmann/askthecrowds/StartScreen.java
+++ src/AndroidClient/src/net/maxhorstmann/askthecrowds/StartScreen.java
@@ -1,15 +1,13 @@
 package net.maxhorstmann.askthecrowds;
 
-import net.maxhorstmann.askthecrowds.services.BackendService;
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class StartScreen extends Activity {
+public class StartScreen extends FragmentActivity {
 	
 	Button button;
 	TextView textView;
@@ -23,64 +21,75 @@ public class StartScreen extends Activity {
 		
 		setContentView(R.layout.activity_start_screen);
 		
-		textView = (TextView)findViewById(R.id.textView1);
-
-		resultDownloader = new ResultDownloader<String>();		
-		resultDownloader.start();
-		resultDownloader.getLooper();
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 		
-		button = (Button)findViewById(R.id.button1);
-		button.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				textView.setText("creating user...");
-				
-				resultDownloader.queueResultDownload(token, pollGuid)
-				
-				//CreateUserTask createUserTask = new CreateUserTask();
-				//createUserTask.execute(textView);
-				
-			}
-		});
-		
-			
-		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.start_screen, menu);
-		return true;
-	}
-	
-	@Override
-	public void onDestroy()
-	{
-		super.onDestroy();
-		resultDownloader.quit();
-	}
-	
-	private class CreateUserTask extends AsyncTask<TextView, Void, String> {
-
-		TextView mTextView;
-		
-		@Override
-		protected String doInBackground(TextView... textView) {
-			mTextView = textView[0];
-			return new BackendService().createUser();
+		if (fragment == null) {
+			fragment = new PollDetailsFragment();
+			fm.beginTransaction()
+			  .add(R.id.fragmentContainer, fragment)
+			  .commit();
 			
 		}
 		
-		@Override
-		protected void onPostExecute(String result) {
-			// runs on the UI thread!
-			mTextView.setText(result);
+		//textView = (TextView)findViewById(R.id.textView1);
+
+		//resultDownloader = new ResultDownloader<String>();		
+		//resultDownloader.start();
+		//resultDownloader.getLooper();
+		
+		//button = (Button)findViewById(R.id.button1);
+//		button.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				
+//				textView.setText("creating user...");
+//				
+//				//resultDownloader.queueResultDownload(token, pollGuid)
+//				
+//				//CreateUserTask createUserTask = new CreateUserTask();
+//				//createUserTask.execute(textView);
+//				
+//			}
+//		});
+		
 			
-		}
 		
 	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.start_screen, menu);
+//		return true;
+//	}
+	
+//	@Override
+//	public void onDestroy()
+//	{
+//		super.onDestroy();
+//		resultDownloader.quit();
+//	}
+	
+//	private class CreateUserTask extends AsyncTask<TextView, Void, String> {
+//
+//		TextView mTextView;
+//		
+//		@Override
+//		protected String doInBackground(TextView... textView) {
+//			mTextView = textView[0];
+//			return new BackendService().createUser();
+//			
+//		}
+//		
+//		@Override
+//		protected void onPostExecute(String result) {
+//			// runs on the UI thread!
+//			mTextView.setText(result);
+//			
+//		}
+//		
+//	}
 
 }
