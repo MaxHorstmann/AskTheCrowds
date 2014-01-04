780a320c764982058629cb8e9173ad8f3e1346c8
Max Horstmann <max.horstmann@gmail.com>
2014-01-03 20:38:57 -0500

    post poll

--- src/AndroidClient/src/net/maxhorstmann/askthecrowds/fragments/CreatePollFragment.java
+++ src/AndroidClient/src/net/maxhorstmann/askthecrowds/fragments/CreatePollFragment.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import net.maxhorstmann.askthecrowds.R;
 import net.maxhorstmann.askthecrowds.models.Poll;
 import net.maxhorstmann.askthecrowds.services.BackendService;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,20 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class CreatePollFragment extends Fragment {
+	
+	private class PostPollTask extends AsyncTask<Poll, Void, String>
+	{
+
+		@Override
+		protected String doInBackground(Poll... params) {
+			BackendService backend = new BackendService();
+			String pollId = backend.postPoll(params[0]);
+			return pollId;
+
+		}		
+		
+	}
+	
 
 	Button mButtonPublish;
 	EditText mEditTextQuestion;
@@ -41,13 +56,16 @@ public class CreatePollFragment extends Fragment {
 			
 			@Override
 			public void onClick(View v) {
-				BackendService backend = new BackendService();
 				Poll poll = new Poll();
 				poll.Question = mEditTextQuestion.getText().toString();
 				poll.Options = new ArrayList<String>();
 				poll.Options.add(mEditTextAnswer1.getText().toString());
 				poll.Options.add(mEditTextAnswer2.getText().toString());
-				backend.postPoll(poll);
+				
+				poll.UserGuid = "929ae5db-76e4-498a-9e8b-81c34838a4a1";
+				
+				PostPollTask postPollTask = new PostPollTask();
+				postPollTask.execute(poll);
 				
 			}
 		});
--- src/AndroidClient/src/net/maxhorstmann/askthecrowds/services/BackendService.java
+++ src/AndroidClient/src/net/maxhorstmann/askthecrowds/services/BackendService.java
@@ -1,13 +1,19 @@
 package net.maxhorstmann.askthecrowds.services;
 
+import java.io.UnsupportedEncodingException;
+
 import net.maxhorstmann.askthecrowds.models.Poll;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
 
 public class BackendService {
 	
@@ -36,22 +42,35 @@ public class BackendService {
 	
 	public String postPoll(Poll poll) {
 		
+		Gson gson = new Gson();
+		String json = gson.toJson(poll);
+		
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpPost post = new HttpPost(baseUrl + "/polls");
+		try {
+			post.setEntity(new StringEntity(json));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		HttpResponse response;
 		
 		try {
 			response = httpclient.execute(post);
 		    StatusLine statusLine = response.getStatusLine();
 		    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-		    	return "created";
+		    	HttpEntity responseEntity = response.getEntity();
+				return responseEntity.toString();
+		    	
+		    			
 		    }
 		} 
 		catch (Exception ex)
 		{
+			ex.printStackTrace();
 		}
 		
-		return "1234-5678-1234-5678";
+		return null;
 		
 		
 	}
