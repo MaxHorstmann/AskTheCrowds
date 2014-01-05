b294e5ffb1fd96119a6f5c0b139a3639e4b8a29e
Max Horstmann <max.horstmann@gmail.com>
2014-01-05 01:14:02 -0500

    breaking the build on purpose

--- src/Server/bin/askthecrowdsserver.dart
+++ src/Server/bin/askthecrowdsserver.dart
@@ -10,6 +10,8 @@ void main() {
   
   HttpServer.bind(InternetAddress.ANY_IP_V4, 80).then((HttpServer server) {
     
+    foobar breaking the build on purpose!!
+    
     var routeTable = new Map<String, Handler>();    
     var homeController = new HomeController();
     var apiController = new ApiController(connectionStringRedis);
