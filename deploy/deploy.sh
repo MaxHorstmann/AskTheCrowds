b6f937b90e52fbc722d5287db3732f4014be4c60
Max Horstmann <max.horstmann@gmail.com>
2014-04-08 22:03:54 -0400

    fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 sudo rm -rf AskTheCrowds
 git clone git@github.com:MaxHorstmann/AskTheCrowds.git
 sudo stop atc-server
 sudo rm -rf /opt/atc-server/*
 sudo cp -r AskTheCrowds/src/Server/* /opt/atc-server/
+cd /opt/atc-server/
 sudo /opt/dart/dart/dart-sdk/bin/pub get
 sudo start atc-server
