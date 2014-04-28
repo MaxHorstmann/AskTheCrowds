6fec2e7609b741a033a992f51a795f5448295b25
Max Horstmann <max.horstmann@gmail.com>
2014-04-27 20:01:10 -0400

    improve img perf

--- src/Server/bin/controllers/ImagesController.dart
+++ src/Server/bin/controllers/ImagesController.dart
@@ -35,10 +35,10 @@ class ImagesController extends BaseController
       request.response.statusCode = HttpStatus.OK;
       request.response.headers.contentType = ContentType.parse("image/jpg");
       var file = new File("C:\\images\\20140328_174533.jpg");
-      file.readAsBytes().then((List<int> bytes) {
-        bytes.forEach((int b) => request.response.writeCharCode(b));
-        request.response.close();       
-      });     
+      var fut = file.readAsBytes();
+      request.response.addStream(fut.asStream()).whenComplete(() {
+        request.response.close();
+      });
       
       return true;
     }
