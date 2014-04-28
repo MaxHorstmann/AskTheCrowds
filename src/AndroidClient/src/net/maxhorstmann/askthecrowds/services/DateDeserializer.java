25a8e2abf6d50db25526652575f7bdb21a0a927d
Max Horstmann <max.horstmann@gmail.com>
2014-04-27 21:23:00 -0400

    fix datetime

--- src/AndroidClient/src/net/maxhorstmann/askthecrowds/services/DateDeserializer.java
+++ src/AndroidClient/src/net/maxhorstmann/askthecrowds/services/DateDeserializer.java
@@ -11,19 +11,29 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
-public class DateDeserializer implements JsonDeserializer<Date> {
+public class DateDeserializer implements JsonDeserializer<Date>, JsonSerializer<Date> {
 
+	static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
+	
 	@Override
 	public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
 		String date = element.getAsString();
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:s", Locale.US);
 		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 		try {
 			return formatter.parse(date);
 		} catch (ParseException e) {
 			return null;
 		}
+	}
+	
+	@Override
+	public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+	  formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+      return src == null ? null : new JsonPrimitive(formatter.format(src));
 	}
 
 }
